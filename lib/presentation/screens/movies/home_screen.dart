1bbfb62bef9e93e379181a754c903ba380662c49
silvaes_GHCO <silvaes@globalhitss.com>
2025-07-10 21:20:08 -0500

    init project

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  /// The name of the HomeScreen route.
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Placeholder());
+  }
+}
